f64ca13c771989f6db6540821caa5540e93b39fc
SurajJCk <you@example.com>
2023-08-29 15:25:48 +0530

    hero, loader

--- src/components/components/Loader.jsx
+++ src/components/components/Loader.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-loader"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+          position: "relative",
+          zIndex: 2,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+      <span
+        className="canvas-loader"
+        style={{ marginTop: 20, zIndex: 1 }}
+      ></span>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
